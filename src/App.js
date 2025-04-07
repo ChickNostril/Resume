b8a5ff95ee989765026d819c0e1bf8283a6b0c88
ChickNostril <chicknostril0626@gmail.com>
2025-04-07 17:50:38 +0900

    [FIX] : HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import ResumePage from "./routes/ResumePage";
 import PortfolioPage from "./routes/PortfolioPage";
